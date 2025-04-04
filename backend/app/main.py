9b144eb025741d995feed868520da352361c4a54
Robert Mesek <rmesek@student.agh.edu.pl>
2025-04-04 07:04:28 +0200

    Add support for CORS origin regexes and update environment configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,6 +25,7 @@ if settings.all_cors_origins:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=settings.all_cors_origins,
+        allow_origin_regex=settings.all_cors_origins_regexes,  # TODO: hack
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
