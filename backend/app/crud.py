a193dc96ffdbdd5b54de7fe3808ebe2f9056786c
Robert Mesek <rmesek@student.agh.edu.pl>
2025-05-12 13:59:48 +0200

    Add promoter profiles endpoints without privileges checking

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -4,7 +4,15 @@ from typing import Any
 from sqlmodel import Session, select
 
 from app.core.security import get_password_hash, verify_password
-from app.models import Item, ItemCreate, User, UserCreate, UserUpdate
+from app.models import (
+    Item,
+    ItemCreate,
+    PromoterProfile,
+    PromoterProfileCreate,
+    User,
+    UserCreate,
+    UserUpdate,
+)
 
 
 def create_user(*, session: Session, user_create: UserCreate) -> User:
@@ -52,3 +60,13 @@ def create_item(*, session: Session, item_in: ItemCreate, owner_id: uuid.UUID) -
     session.commit()
     session.refresh(db_item)
     return db_item
+
+
+def create_promoter_profile(
+    *, session: Session, promoter_profile_create: PromoterProfileCreate
+) -> PromoterProfile:
+    db_obj = PromoterProfile.model_validate(promoter_profile_create)
+    session.add(db_obj)
+    session.commit()
+    session.refresh(db_obj)
+    return db_obj
