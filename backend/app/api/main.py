1a579f28b816f1854e2405d8c6399388ef332922
Robert Mesek <rmesek@student.agh.edu.pl>
2025-05-12 21:20:54 +0200

    Add basic endpoints (untested)

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,16 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, private, promoter_profiles, users, utils
+from app.api.routes import (
+    items,
+    login,
+    private,
+    promoter_profiles,
+    student_profiles,
+    thesis_applications,
+    thesis_topics,
+    users,
+    utils,
+)
 from app.core.config import settings
 
 api_router = APIRouter()
@@ -9,6 +19,9 @@ api_router.include_router(users.router)
 api_router.include_router(utils.router)
 api_router.include_router(items.router)
 api_router.include_router(promoter_profiles.router)
+api_router.include_router(student_profiles.router)
+api_router.include_router(thesis_applications.router)
+api_router.include_router(thesis_topics.router)
 
 
 if settings.ENVIRONMENT == "local":
